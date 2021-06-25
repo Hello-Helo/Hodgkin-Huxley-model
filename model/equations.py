97f84aac6bf4b1a5113743c5ea0ce033986bad39
Hello-Helo <heloisalbento@gmail.com>
2021-06-25 11:20:48 -0300

    Add equations.py

---
+++ model/equations.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def alpha_n(voltage):
+    a = (0.01*(10+voltage))/(np.exp((10+voltage)/10)-1)
+    return a
+
+def alpha_m
+
+
+def alpha_h
+
+
+def beta_n
+
+
+def beta_m
+
+
+def beta_h
